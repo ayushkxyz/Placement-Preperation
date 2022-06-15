130a2a0d7c389a474f0f5bc5e28b23faeb435dad
Ayush Katiyar <89083667+ayushkxyz@users.noreply.github.com>
2022-06-15 16:33:08 +0530

    Update Searching an element in a sorted array.cpp

--- Searching/Searching an element in a sorted array.cpp
+++ Searching/Searching an element in a sorted array.cpp
@@ -1,6 +1,6 @@
 int searchInSorted(int arr[], int N, int K) 
     { 
-    
+      //Binary search Iteratively
        // Your code here
        int low=0,high=N-1;
        int mid;
@@ -18,3 +18,28 @@ int searchInSorted(int arr[], int N, int K)
        }
        return -1;
     }
+
+// Binary search Recursively
+
+    int helper(int input[],int low,int high,int element){
+    if(low>high){
+        return -1;
+    }
+    int mid=(low+high)/2;
+      if(input[mid]==element){
+          return mid;
+      }
+    else if(input[mid]>element){
+        return helper(input,low,mid-1,element);
+    }
+    else{
+        return helper(input,mid+1,high,element);
+    }
+}
+
+
+int binarySearch(int input[], int size, int element) {
+    // Write your code here
+    return helper(input,0,size-1,element);
+
+}
